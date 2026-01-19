7ef2138b80d6594345c0bfcb08219ca2db48a84c
Pascal Fodiman <pascal@fodiman.fr>
2026-01-19 09:26:32 +0100

    improve home page with records count per gender

--- app/models/gender.rb
+++ app/models/gender.rb
@@ -1,4 +1,9 @@
 class Gender < ApplicationRecord
   has_and_belongs_to_many :records
   validates :name, presence: true, uniqueness: true
+
+  def number_of_records_per_gender(id)
+    # returns number of records associated to Gender(id)
+    Gender.find(id).records.count
+  end
 end
