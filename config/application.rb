83d650bc417025c786169ce6f092e32fd0ea2d50
Pascal Fodiman <pascal@fodiman.fr>
2026-08-26 22:37:51 +0200

    Load Rails 8.0 defaults

--- config/application.rb
+++ config/application.rb
@@ -15,8 +15,7 @@ module Vidb
       generate.test_framework :test_unit, fixture: false
     end
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 7.2
-    config.active_support.to_time_preserves_timezone = :zone
+    config.load_defaults 8.0
 
     # Please, add to the `ignore` list any other `lib` subdirectories that do
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
