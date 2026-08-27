e97f7881f0f7e4f641245c2d91ff35cf713cdef4
Pascal Fodiman <pascal@fodiman.fr>
2026-08-27 10:07:41 +0200

    Configure production mailer URLs

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,7 +1,11 @@
 require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
-  config.action_mailer.default_url_options = { host: "http://TODO_PUT_YOUR_DOMAIN_HERE" }
+  config.action_mailer.default_url_options = {
+    host: ENV.fetch("VIDB_HOST", "zeus.fodiman.fr"),
+    protocol: "https"
+  }
+
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
