f82ad5f7fc4dbaf473a72af548cc9a73464ea521
Pascal Fodiman <pascal@fodiman.fr>
2026-09-11 00:02:03 +0200

    Search hierarchy parents from move page

--- app/controllers/record_hierarchy_placements_controller.rb
+++ app/controllers/record_hierarchy_placements_controller.rb
@@ -2,6 +2,7 @@ class RecordHierarchyPlacementsController < ApplicationController
   before_action :set_record
 
   def edit
+    prepare_parent_search(@record)
   end
 
   def update
@@ -13,11 +14,20 @@ class RecordHierarchyPlacementsController < ApplicationController
         notice: "La fiche a été déplacée avec succès."
       )
     else
+      prepare_parent_search(Record.find(@record.id))
       render :edit, status: :unprocessable_content
     end
   end
 
   private
+
+  def prepare_parent_search(search_record)
+    @query = params[:q].to_s
+    @candidate_parents = RecordHierarchyParentSearch
+                         .new(record: search_record, query: @query)
+                         .results
+                         .page(params[:page])
+  end
 
   def requested_parent
     parent_id = hierarchy_placement_params.fetch(:parent_id)
@@ -33,5 +43,6 @@ class RecordHierarchyPlacementsController < ApplicationController
   def set_record
     @record = Record.find(params[:record_id])
     @current_parent = @record.parent
+    @descendant_count = @record.descendants.count
   end
 end
--- test/controllers/record_hierarchy_placements_controller_test.rb
+++ test/controllers/record_hierarchy_placements_controller_test.rb
@@ -1,7 +1,6 @@
 require "test_helper"
 
-class RecordHierarchyPlacementsControllerTest <
-    ActionDispatch::IntegrationTest
+class RecordHierarchyPlacementsControllerTest < ActionDispatch::IntegrationTest
   include Devise::Test::IntegrationHelpers
 
   setup do
@@ -41,6 +40,7 @@ class RecordHierarchyPlacementsControllerTest <
     get edit_record_hierarchy_placement_url(@season)
 
     assert_response :success
+    assert_select "[data-hierarchy-parent-candidate]", count: 0
     assert_select "[data-hierarchy-record]", text: /Saison 1/
     assert_select(
       "[data-hierarchy-current-parent]",
@@ -233,5 +233,85 @@ class RecordHierarchyPlacementsControllerTest <
 
     assert_response :bad_request
     assert_equal original_parent, @season.reload.parent
+  end
+
+  test "displays matching parent candidates with useful context" do
+    destination = Record.create!(
+      french_title: "Série destination",
+      original_title: "Destination Series",
+      record_kind: "series",
+      year: 2020,
+      language_version: @series.language_version
+    )
+
+    get edit_record_hierarchy_placement_url(@season), params: {
+      q: "destination"
+    }
+
+    assert_response :success
+    assert_select(
+      "form[action='#{edit_record_hierarchy_placement_path(@season)}']"
+    )
+    assert_select(
+      "input[name='q'][value='destination']"
+    )
+
+    assert_select(
+      "[data-hierarchy-parent-candidate='#{destination.id}']"
+    ) do
+      assert_select "[data-candidate-title]",
+                    text: /Série destination/
+      assert_select "[data-candidate-kind]",
+                    text: /Série/
+      assert_select "[data-candidate-year]",
+                    text: /2020/
+      assert_select "[data-candidate-ancestry]",
+                    text: /Racine/
+
+      assert_select(
+        "form[action='#{record_hierarchy_placement_path(@season)}']" \
+        "[data-turbo-confirm]"
+      ) do
+        assert_select(
+          "input[name='_method'][value='patch']"
+        )
+        assert_select(
+          "input[name='hierarchy_placement[parent_id]']" \
+          "[value='#{destination.id}']"
+        )
+        assert_select "button", text: /Déplacer ici/
+      end
+    end
+  end
+  
+  test "paginates matching parent candidates" do
+    27.times do |index|
+      Record.create!(
+        french_title: format("Parent paginé %02d", index),
+        record_kind: "series",
+        language_version: @series.language_version
+      )
+    end
+
+    get edit_record_hierarchy_placement_url(@season), params: {
+      q: "Parent paginé"
+    }
+
+    assert_response :success
+    assert_equal(
+      26,
+      css_select("[data-hierarchy-parent-candidate]").size
+    )
+
+    get edit_record_hierarchy_placement_url(@season), params: {
+      q: "Parent paginé",
+      page: 2
+    }
+
+    assert_response :success
+    assert_equal(
+      1,
+      css_select("[data-hierarchy-parent-candidate]").size
+    )
   end
 end
